a22033d753e77cefc7a9ef9b83d0f4e70156a1c5
Lucas Wang <lwangprivate@gmail.com>
2020-08-09 21:35:08 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const session = require("express-session")
+const hbs = require("hbs")
+const path = require("path")
+
+const app = express()
+
+// Directories
+const partialsDir = path.join(__dirname, "/templates/partials")
+const viewsDir = path.join(__dirname, "/templates/views")
+const publicDir = path.join(__dirname, "/public")
+
+// Modules
+hbs.registerPartials(partialsDir)
+app.set("view engine", "hbs")
+app.set("views", viewsDir)
+app.use(express.static(publicDir))
+app.use(express.json())
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false
+    },
+    secret: process.env.COOKIE_SECRET
+}))
+
+// Routers
+const navRouter = require("./routers/nav")
+app.use(navRouter)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log("Listening on port " + port + ".")
+})
---
+++ routers/nav.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const path = require("path")
+
+const navRouter = express.Router()
+
+navRouter.get("/robots.txt", (req, res) => {
+   res.sendFile(path.join(__dirname, "../robots.txt"))
+})
+
+navRouter.get("/", (req, res) => {
+    res.redirect("/home")
+})
+
+navRouter.get("/home", (req, res) => {
+    res.render("home")
+})
+
+module.exports = navRouter
