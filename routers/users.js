dedd872d0a2f30cebcf3db577d0305ab78e854eb
Lucas Wang <lwangprivate@gmail.com>
2020-08-10 18:58:36 -0500

    Testing user registration and login.

--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const User = require("../models/user")
+const auth = require("../middleware/auth")
 
 const userRouter = express.Router()
 
@@ -8,6 +9,7 @@ userRouter.post("/register", async (req, res) => {
         let user = new User(req.body)
         await user.save()
         let token = await user.generateAuthToken()
+        req.session.token = token
         res.status(200).send({user, token})
     } catch (error) {
         if (!error.errors) { return res.status(400).send({error: "An unexpected problem occurred."}) }
@@ -22,11 +24,16 @@ userRouter.post("/login", async (req, res) => {
             throw new Error("Username or password is incorrect.")
         }
         let token = await user.generateAuthToken()
+        req.session.token = token
         res.status(200).send({user, token})
     } catch (error) {
         if (!error.errors) { return res.status(400).send({error: "An unexpected problem occurred."}) }
         res.status(400).send({error: error.errors})
     }
+})
+
+userRouter.get("/get-username", auth, (req, res) => {
+    return {username: req.user.username}
 })
 
 module.exports = userRouter
