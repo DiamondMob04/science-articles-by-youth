92e6c642977adf9663132ae63068cb9a0b842a5e
Lucas Wang <lwangprivate@gmail.com>
2020-08-11 15:07:53 -0500

    Fixed bug where there were less than 9 users in the Members tab.

--- public/js/members.js
+++ public/js/members.js
@@ -5,14 +5,15 @@ async function fetchMembers() {
     try {
         let response = await fetch(`/users?skip=${currentSkip}&limit=${limitation}`)
         let json = await response.json()
-        if (json.users.length == 0) {
+        if (currentSkip == 0 && json.users.length == 0) {
             $("#err-message").html(`Could not find any users.`)
         }
         if (!json.moreUsers) {
-            $(".find-more").css({ transform: "scale(1)", background: "gray" })
+            $(".find-more").css({ transform: "scale(1)", background: "gray" }).attr("disabled", true)
         } else {
             currentSkip += limitation
         }
+        console.log(json)
         for (let i = 0; i < json.users.length; i++) {
             let user = json.users[i]
             $(".triple-threat").append(`<div class="member-block" onclick="window.location.href = '/user/${user.username}'"><h2 class="prof-username">${user.username}</h2><h4 class="prof-role">${user.role}</h4><img class="prof-avatar" src="../img/avatar.jpg"></div>`)
