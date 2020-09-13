4f9df982d560b442a5953e763578847ec65003b3
Lucas Wang <lwangprivate@gmail.com>
2020-09-13 17:19:57 -0500

    Revised search bar.

--- public/js/article-filter.js
+++ public/js/article-filter.js
@@ -1,20 +1,20 @@
-$(document).keyup((event) => {
-    if ($("#filter-keywords").is(":focus") && event.key == "Enter") {
-        const keywords = $("#filter-keywords").val().replace(/[^0-9a-zA-Z\s]/g, "")
-        const currPage = window.location.href.split("/").pop().split("?")[0]
-        if (keywords.trim() === "") {
-            return window.location.href = currPage
-        }
-        return window.location.href = `/${currPage}?tags=${keywords}`
-    }
-})
-
-// When the filter button is clicked.
-$("#filter-submit").click(() => {
+function refreshKeywords() {
     const keywords = $("#filter-keywords").val().replace(/[^0-9a-zA-Z\s]/g, "")
     const currPage = window.location.href.split("/").pop().split("?")[0]
     if (keywords.trim() === "") {
         return window.location.href = currPage
     }
     return window.location.href = `/${currPage}?tags=${keywords}`
+}
+
+// When the enter key is pressed and you're selecting the text field.
+$(document).keyup((event) => {
+    if ($("#filter-keywords").is(":focus") && event.key == "Enter") {
+        refreshKeywords()
+    }
+})
+
+// When the filter button is clicked.
+$("#filter-submit").click(() => {
+    refreshKeywords()
 })
